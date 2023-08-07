0ea7292c87bdd7bee1b65863f4220a33bd1791ef
Matt <git@mattthenub.dev>
2023-08-07 20:31:47 +0200

    add a sick redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,4 +16,10 @@ exports.createPages = async ({ graphql, actions }) => {
 		fromPath: '/sortdle',
 		toPath: 'https://sortdle.mattthenub.dev/',
 	})
+
+	actions.createRedirect({
+		fromPath: '/nuzlocke',
+		toPath:
+			'https://docs.google.com/document/d/1H-MC7KriA16hXl9rN_K3rvu5-vbWILVnFrHeU54tnH0/edit?usp=sharing',
+	})
 }
